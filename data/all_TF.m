397db608d088ed9bea014966084a1d1096a5c6a7
1ecin1 <39767025+1ecin1@users.noreply.github.com>
2024-11-25 08:50:52 +0800

    Update all_TF.m

--- data/all_TF.m
+++ data/all_TF.m
@@ -1,34 +1,3 @@
-load('Freq.mat')
-%%
-f=Freq(1:3000,:);
-%%
-figure
-imagesc(1:size(result_6_5_22,2),f,result_6_5_22)
-%%
-up_a=result_6_5_22(:,400:430);
-up_b=result_6_5_22(:,250:280);
-up_c=result_6_5_22(:,50:80);
-
-do_a=result_6_5_29(:,400:430);
-do_b=result_6_5_29(:,250:280);
-do_c=result_6_5_29(:,50:80);
-%%
-figure
-imagesc(1:size(result_6_7_22,2),f,result_6_7_22)
-%%
-up_d=result_6_7_22(:,260:290);
-up_e=result_6_7_22(:,170:200);
-up_f=result_6_7_22(:,50:80);
-
-do_d=result_6_7_29(:,260:290);
-do_e=result_6_7_29(:,170:200);
-do_f=result_6_7_29(:,50:80);
-%%
-up_all=[zeros(3000, 2) up_a zeros(3000, 5) up_b zeros(3000, 7) up_d zeros(3000, 10) up_c zeros(3000, 14) up_e zeros(3000, 20) up_f zeros(3000, 2)];
-%up_all(up_all==0)=10;
-do_all=[zeros(3000, 2) do_a zeros(3000, 5) do_b zeros(3000, 7) do_d zeros(3000, 10) do_c zeros(3000, 14) do_e zeros(3000, 20) do_f zeros(3000, 2)]+10;
-
-%%
 figure('name','总体频域5','NumberTitle', 'off','Position',[200 300 700 300])
 %imagesc(1:size(up_all,2),f,up_all)
 pcolor(1:size(up_all,2),f,up_all)

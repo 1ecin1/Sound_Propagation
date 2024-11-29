ce525b1fcdcd3d56178aca24c085d5df70c13538
1ecin1 <39767025+1ecin1@users.noreply.github.com>
2024-11-29 10:08:21 +0800

    Delete core directory

--- core/abs_fft.m
+++
@@ -1,7 +0,0 @@
-%逆傅里叶
-temp_fx=ifft(fk);
-temp_fx=temp_fx(1 : N+1);
-temp_fx(1)=0.000000000000000 + 0.000000000000000i;
-temp_fx(N+1)=0.000000000000000 + 0.000000000000000i;
-%逆插值
-starter_temp = interp1(dep{m,1},phi{1,1},zd,    'linear');
